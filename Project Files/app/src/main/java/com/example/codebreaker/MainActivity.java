1b38dbb60012aa85c9b22458844c2448f9f4162a
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-02-16 15:18:30 -0500

    Added second activity and peg images

--- Project Files/app/src/main/java/com/example/codebreaker/MainActivity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.codebreaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configurePushButton();
+    }
+
+    private void configurePushButton(){
+        Button pushButton = findViewById(R.id.playButton);
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                startActivity(new Intent(MainActivity.this, Main3Activity.class));
+            }
+        });
     }
 }
