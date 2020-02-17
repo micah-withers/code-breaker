93a4dc3a4ff1ad654b74ea69ad632d1032ec542d
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-02-17 12:54:02 -0500

    Tried making test.

--- Project Files/app/src/androidTest/java/com/example/codebreaker/MainActivityTest.java
+++ Project Files/app/src/androidTest/java/com/example/codebreaker/MainActivityTest.java
@@ -1,18 +1,30 @@
 package com.example.codebreaker;
 
+import android.content.Intent;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 
-//import static androidx.core.graphics.drawable.IconCompat.getResources;
+
+//import static androidx.core.content.ContextCompat.startActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-//import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 //import static org.junit.Assert.*;
-//import android.view.View;
+
 
 
 public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void onCreate() {
