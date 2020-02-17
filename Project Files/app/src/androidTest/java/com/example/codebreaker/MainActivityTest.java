29bf9499476520f25ea88c987d9dea8cf130a81c
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-02-17 08:13:52 -0500

    Create MainActivityTest.java

---
+++ Project Files/app/src/androidTest/java/com/example/codebreaker/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.codebreaker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+
+    }
+}
