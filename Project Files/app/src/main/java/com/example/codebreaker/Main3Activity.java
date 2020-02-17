d52c01c8efff2af27e6bea9e79ab611152a9dbf7
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-02-16 23:26:38 -0500

    Created array of drawables

--- Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
@@ -2,6 +2,8 @@ package com.example.codebreaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,35 +11,43 @@ import android.widget.Button;
 import java.util.ArrayList;
 
 public class Main3Activity extends AppCompatActivity {
-    private ArrayList<android.view.View> pegList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        pegList.add(findViewById(R.id.masterPeg1));
-        pegList.add(findViewById(R.id.masterPeg2));
-        pegList.add(findViewById(R.id.masterPeg3));
-        pegList.add(findViewById(R.id.masterPeg4));
-
-        for (android.view.View peg: pegList) {
-            peg.setVisibility(View.INVISIBLE);
-        }
-
-        setContentView(R.layout.activity_main3);
-
-        configurePushButton();
+//        TypedArray pegList = getResources().obtainTypedArray(drawable.id.pegs);
+//        android.view.View
+//
+//
+//        Drawable peg;
+//        for (int i = 0; i < 5; ++i) {
+//            peg = pegList.getDrawable(i);
+//            peg.setVisibility(android.view.View.INVISIBLE);
+//        }
+//
+//        setContentView(R.layout.activity_main3);
+//
+//        configurePushButton();
+//        pegList.recycle();
     }
 
-    private void configurePushButton(){
-        Button pushButton = findViewById(R.id.playButton);
-        pushButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                for (android.view.View peg: pegList) {
-                    peg.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-    }
+//    private void configurePushButton(){
+//        Button pushButton = findViewById(R.id.playButton);
+//        pushButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//                android.view.View peg;
+//                for (android.view.View i: pegList) {
+//                    peg = i;
+//                    peg.setVisibility(View.VISIBLE);
+//                }
+//            }
+//        });
+//    }
+//
+//    private ArrayList<> generateCode() {
+//        TypedArray[] code;
+//
+//    }
 }
