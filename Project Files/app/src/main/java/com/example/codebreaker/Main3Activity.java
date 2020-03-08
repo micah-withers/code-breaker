ccdeb7e75cd09a8c148167aef863953cce060cd1
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-03-08 18:59:54 -0400

    Changed some to view binding
    Button does not work with view binding

--- Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 //import android.content.res.TypedArray;
 //import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.codebreaker.databinding.ActivityMain3Binding;
 //import android.view.View;
 //import android.widget.Button;
 
@@ -12,42 +15,16 @@ import android.os.Bundle;
 
 public class Main3Activity extends AppCompatActivity {
 
+    private ActivityMain3Binding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        TypedArray pegList = getResources().obtainTypedArray(drawable.id.pegs);
-//        android.view.View
-//
-//
-//        Drawable peg;
-//        for (int i = 0; i < 5; ++i) {
-//            peg = pegList.getDrawable(i);
-//            peg.setVisibility(android.view.View.INVISIBLE);
-//        }
-//
-        setContentView(R.layout.activity_main3);
-//
-//        configurePushButton();
-//        pegList.recycle();
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
-//    private void configurePushButton(){
-//        Button pushButton = findViewById(R.id.playButton);
-//        pushButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                // Code here executes on main thread after user presses button
-//                android.view.View peg;
-//                for (android.view.View i: pegList) {
-//                    peg = i;
-//                    peg.setVisibility(View.VISIBLE);
-//                }
-//            }
-//        });
-//    }
-//
-//    private ArrayList<> generateCode() {
-//        TypedArray[] code;
-//
-//    }
+
+
 }
