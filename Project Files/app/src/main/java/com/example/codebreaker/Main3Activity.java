8dc361b0dc5c4cdb7195d3e0985d864844824592
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-03-08 20:47:32 -0400

    Changed OnLong..Listener to method
    Still cannot create parent directory

--- Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
@@ -3,46 +3,40 @@ package com.example.codebreaker;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.content.res.TypedArray;
-//import android.graphics.drawable.Drawable;
 import android.content.ClipData;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
 
 import com.example.codebreaker.databinding.ActivityMain3Binding;
-//import android.view.View;
-//import android.widget.Button;
-
-//import java.util.ArrayList;
 
 public class Main3Activity extends AppCompatActivity {
-
-    private ActivityMain3Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        com.example.codebreaker.databinding.ActivityMain3Binding binding = ActivityMain3Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.taskBar1.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar1.setOnLongClickListener(createLongClickListener());
         binding.box11.setOnDragListener(new MyDragListener());
     }
 
-    private class MyLongClickListener implements View.OnLongClickListener {
-        @RequiresApi(api = Build.VERSION_CODES.N)
-        public boolean onLongClick(View v) {
-            ClipData data = ClipData.newPlainText("", "");
-            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
-            v.startDragAndDrop(data, shadowBuilder, v, 0);
-            return true;
-        }
+    private View.OnLongClickListener createLongClickListener() {
+        return new View.OnLongClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            public boolean onLongClick(View v) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+                v.startDragAndDrop(data, shadowBuilder, v, 0);
+                return true;
+            }
+        };
     }
+
 
     private static class MyDragListener implements View.OnDragListener {
 
