5adedcdb3788a8900b378140e7cef1f257bc397c
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-03-08 23:20:00 -0400

    Added all pegs and 4 boxes to drop (not functional yet)

--- Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
@@ -13,50 +14,58 @@ import android.view.View;
 import com.example.codebreaker.databinding.ActivityMain3Binding;
 
 public class Main3Activity extends AppCompatActivity {
-
+    com.example.codebreaker.databinding.ActivityMain3Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.example.codebreaker.databinding.ActivityMain3Binding binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        binding.taskBar1.setOnLongClickListener(createLongClickListener());
+        binding.taskBar1.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar2.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar3.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar4.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar5.setOnLongClickListener(new MyLongClickListener());
+        binding.taskBar6.setOnLongClickListener(new MyLongClickListener());
         binding.box11.setOnDragListener(new MyDragListener());
+        binding.box12.setOnDragListener(new MyDragListener());
+        binding.box13.setOnDragListener(new MyDragListener());
+        binding.box14.setOnDragListener(new MyDragListener());
     }
 
-    private View.OnLongClickListener createLongClickListener() {
-        return new View.OnLongClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            public boolean onLongClick(View v) {
-                ClipData data = ClipData.newPlainText("", "");
-                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
-                v.startDragAndDrop(data, shadowBuilder, v, 0);
-                return true;
-            }
-        };
+    private static class MyLongClickListener implements View.OnLongClickListener {
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDragAndDrop(data, shadowBuilder, v, 0);
+            return true;
+        }
     }
 
+    private class MyDragListener implements View.OnDragListener {
 
-    private static class MyDragListener implements View.OnDragListener {
-
-        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int action = event.getAction();
+            View view = (View) event.getLocalState();
+            Drawable original = view.getBackground();
             switch (action) {
             case DragEvent.ACTION_DRAG_STARTED:
                 // nada
+                view.setBackground(getDrawable(R.drawable.highlight));
                 break;
             case DragEvent.ACTION_DRAG_ENTERED:
-                // wait
+                // something
                 break;
             case DragEvent.ACTION_DROP:
-                View view = (View) event.getLocalState();
-                view.setBackground(v.getBackground());
+                v.setBackground(view.getBackground());
                 break;
             case DragEvent.ACTION_DRAG_ENDED:
                 // nothing
+                view.setBackground(original);
                 break;
             default:
                 break;
