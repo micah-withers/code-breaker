ea2e1a5494ee358b42b9e61ea97a1ed1cffb9390
micah-withers <55194246+micah-withers@users.noreply.github.com>
2020-03-28 22:59:29 -0400

    Trying to configure buttons
    Should appear once 4 slots are filled, then the row should be unaccessible and the next row should be accessible.

--- Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main3Activity.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
+import android.widget.Button;
 
 
 import com.example.codebreaker.databinding.ActivityMain3Binding;
@@ -18,29 +19,41 @@ import java.util.ArrayList;
 public class Main3Activity extends AppCompatActivity {
     com.example.codebreaker.databinding.ActivityMain3Binding binding;
     ArrayList<View> boxes;
+    ArrayList<Button> checks;
     private int currentRow, nRows, boxesFilled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        boxes = new ArrayList<View>();
-        int currentRow = 0;
-        int nRows = 2;
-        int boxesFilled = 0;
+        boxes = new ArrayList<>();
+        checks = new ArrayList<>();
+        currentRow = 0;
+        nRows = 2;
+        boxesFilled = 0;
 
         binding = ActivityMain3Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        setContentView(view);
 
         boxes.add(binding.box11);
         boxes.add(binding.box12);
         boxes.add(binding.box13);
         boxes.add(binding.box14);
+
         boxes.add(binding.box21);
         boxes.add(binding.box22);
         boxes.add(binding.box23);
         boxes.add(binding.box24);
+
+        checks.add(binding.check1);
+        checks.add(binding.check2);
+
+        View check;
+
+        for (int i = 0; i < nRows; ++i) {
+            check = checks.get(i);
+            check.setVisibility(View.INVISIBLE);
+        }
 
         binding.taskBar1.setOnLongClickListener(new MyLongClickListener());
         binding.taskBar2.setOnLongClickListener(new MyLongClickListener());
@@ -49,14 +62,31 @@ public class Main3Activity extends AppCompatActivity {
         binding.taskBar5.setOnLongClickListener(new MyLongClickListener());
         binding.taskBar6.setOnLongClickListener(new MyLongClickListener());
 
-        for (View box: boxes) {
-            box.setOnDragListener(new MyDragListener());
-        }
+//        binding.check1
 
-//        binding.box11.setOnDragListener(new MyDragListener());
-//        binding.box12.setOnDragListener(new MyDragListener());
-//        binding.box13.setOnDragListener(new MyDragListener());
-//        binding.box14.setOnDragListener(new MyDragListener());
+        nextDragListeners(currentRow);
+        nextCheckButton(currentRow, boxesFilled);
+
+        setContentView(view);
+    }
+
+    private void nextCheckButton(final int row,final int boxesFilled) {
+        if (row > 0) {
+            Button oldCheck = checks.get(row-1);
+            oldCheck.setOnClickListener(null);
+            oldCheck.setVisibility(View.GONE);
+        }
+        if (row < nRows) {
+            Button newCheck = checks.get(row);
+            newCheck.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    if (boxesFilled == 4) {
+                        nextDragListeners(row);
+                    }
+                }
+            });
+        }
     }
 
     private static class MyLongClickListener implements View.OnLongClickListener {
@@ -71,46 +101,63 @@ public class Main3Activity extends AppCompatActivity {
 
     private class MyDragListener implements View.OnDragListener {
         private boolean hasPeg = false;
+
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int action = event.getAction();
             View view = (View) event.getLocalState();
-//            Drawable original = v.getBackground();
+
             switch (action) {
             case DragEvent.ACTION_DRAG_STARTED:
-                // nada
-//                original = v.getBackground();
                 v.setVisibility(View.VISIBLE);
-//                v.setBackground(getDrawable(R.drawable.highlight));
+                if (!hasPeg) {
+                    v.setBackground(getDrawable(R.drawable.highlight));
+                }
                 break;
-            case DragEvent.ACTION_DRAG_ENTERED:
-                // something
-//                v.setBackground(getDrawable(R.drawable.highlight));
-                break;
+//            case DragEvent.ACTION_DRAG_ENTERED:
+//                if (!hasPeg) {
+//                    v.setBackground(getDrawable(R.drawable.highlight));
+//                }
+//                break;
             case DragEvent.ACTION_DROP:
                 v.setBackground(view.getBackground());
                 if (!hasPeg) {
+                    hasPeg = true;
                     ++boxesFilled;
+                    if (boxesFilled == 4) {
+                        View check = checks.get(currentRow);
+                        check.setVisibility(View.VISIBLE);
+                        ++currentRow;
+                        nextCheckButton(currentRow, boxesFilled);
+                        boxesFilled = 0;
+                    }
                 }
-                hasPeg = true;
-                break;
-            case DragEvent.ACTION_DRAG_EXITED:
-                // nothing
-//                v.setBackground(original);
-//                if (v.getBackground() == getDrawable(R.drawable.highlight)) {
-//                    v.setVisibility(View.INVISIBLE);
-//                }
                 break;
             case DragEvent.ACTION_DRAG_ENDED:
-                // idk
-//                if (v.getBackground() == getDrawable(R.drawable.highlight)) {
-//                    v.setVisibility(View.INVISIBLE);
-//                }
+                if (!hasPeg) {
+                    v.setVisibility(View.INVISIBLE);
+                }
+                break;
             default:
                 break;
             }
             return true;
+        }
+    }
+
+    private void nextDragListeners(int row) {
+        if (row > 0) {
+            for (int i = row*4; i < row*4 + 4; ++i) {
+                View box = boxes.get(i);
+                box.setOnDragListener(null);
+            }
+        }
+        if (row < nRows) {
+            for (int i = row*4; i < row*4 + 4; ++i) {
+                View box = boxes.get(i);
+                box.setOnDragListener(new MyDragListener());
+            }
         }
     }
 }
