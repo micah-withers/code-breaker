f3ebb25c9f19496ff73d578305d0f3b7bead058d
excaliburnt <marshallbrown12@yahoo.com>
2020-04-14 20:24:04 -0400

    Added two new activies w buttons and text

--- Project Files/app/src/main/java/com/example/codebreaker/Main2Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main2Activity.java
@@ -7,28 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.codebreaker.databinding.ActivityMainBinding;
+import com.example.codebreaker.databinding.ActivityMain2Binding;
 
-public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+public class Main2Activity extends AppCompatActivity {
+    private ActivityMain2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         configurePushButton();
     }
-
     private void configurePushButton(){
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
         Button pushButton = findViewById(R.id.play1Button);
         pushButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                startActivity(new Intent(Main2Activity.this, Main3Activity.class));
             }
         });
     }
--- Project Files/app/src/main/java/com/example/codebreaker/Main4Activity.java
+++ Project Files/app/src/main/java/com/example/codebreaker/Main4Activity.java
@@ -7,28 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.codebreaker.databinding.ActivityMainBinding;
+import com.example.codebreaker.databinding.ActivityMain4Binding;
 
-public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+public class Main4Activity extends AppCompatActivity {
+    private ActivityMain4Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         configurePushButton();
     }
-
     private void configurePushButton(){
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
         Button pushButton = findViewById(R.id.play1Button);
         pushButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                startActivity(new Intent(Main4Activity.this, Main3Activity.class));
             }
         });
     }
